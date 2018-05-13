7d4ec79e038cdffa2d5eb00cf34c050f4dc96b99
Davide Specchia <davide.specchia.external@ynap.com>
2018-05-13 14:39:25 +0200

    Tests added

--- src/timeline.ts
+++ src/timeline.ts
@@ -1,7 +1,25 @@
+import * as d3 from "d3-interpolate";
 import { IKeyframe } from "./models/keyframe";
 
 export class Timeline {
     constructor(
         private keyframes: IKeyframe[],
         private easingFunction: (x: number) => number = (x) => x) { }
+
+    public getAt(perc: number): IKeyframe {
+        perc = this.normalizePerc(perc);
+        const [k1, k2] = this.getKeyframesCouple(perc);
+        const finalPerc = (perc - k1.time) * (1 / (k2.time - k1.time));
+        return d3.interpolate(k1, k2)(this.easingFunction(finalPerc));
+    }
+
+    private getKeyframesCouple(perc: number) {
+        const startKeyframeIndex = this.keyframes
+            .findIndex((keyframe, i, arr) => keyframe.time <= perc && arr[i + 1].time >= perc);
+        return [this.keyframes[startKeyframeIndex], this.keyframes[startKeyframeIndex + 1]];
+    }
+
+    private normalizePerc(p: number) {
+        return p < 0 ? 0 : p > 1 ? 1 : p;
+    }
 }
--- test/timeline.spec.ts
+++ test/timeline.spec.ts
@@ -1,3 +1,45 @@
-describe("Timeline", () => {
+import { Timeline } from "../src/index";
 
-})
+const keyframes = [{
+    time: 0, value: {
+        n: 0
+    }
+}, {
+    time: 1, value: {
+        n: 1
+    }
+}];
+
+describe("Timeline", () => {
+    describe("getAt", () => {
+        test("should get the right interpolated keyframe at given time", () => {
+            const timeline = new Timeline(keyframes);
+
+            expect(timeline.getAt(0)).toEqual({ time: 0, value: { n: 0 } });
+            expect(timeline.getAt(1)).toEqual({ time: 1, value: { n: 1 } });
+            expect(timeline.getAt(0.5)).toEqual({ time: 0.5, value: { n: 0.5 } });
+        });
+
+        test("should get the first keyframe if the time is lesser then it", () => {
+            const timeline = new Timeline(keyframes);
+
+            expect(timeline.getAt(-1)).toEqual({ time: 0, value: { n: 0 } });
+        });
+
+        test("should get the last keyframe if the time is greater then it", () => {
+            const timeline = new Timeline(keyframes);
+
+            expect(timeline.getAt(2)).toEqual({ time: 1, value: { n: 1 } });
+        });
+
+        test("should get the right keyframe using the provided easing function", () => {
+            const mockEasing = jest.fn();
+            mockEasing.mockReturnValue(1);
+            const timeline = new Timeline(keyframes, mockEasing);
+
+            expect(timeline.getAt(0.5)).toEqual({ time: 1, value: { n: 1 } });
+            expect(mockEasing).toHaveBeenCalledWith(0.5);
+            expect(mockEasing).toHaveBeenCalledTimes(1);
+        });
+    });
+});
